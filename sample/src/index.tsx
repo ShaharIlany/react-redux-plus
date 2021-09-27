4b760966abcf8624f1a48dbefdba0da7e947cbc9
Shahar Ilany <shahar@sps.co.il>
2021-09-27 20:48:27 +0300

    remove provider (not needed)

--- sample/src/index.tsx
+++ sample/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { initializeStore } from 'react-redux-plus'
-import { Provider } from 'react-redux';
 
 const initialStore = {
   paramA: "Test",
@@ -12,10 +11,8 @@ const store = initializeStore(initialStore)
 export type storeType = typeof initialStore
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
