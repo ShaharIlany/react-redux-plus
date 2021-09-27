8891a436c4eb2cb1a4e2735a17553603eb010817
Shahar Ilany <shahar@sps.co.il>
2021-09-27 16:58:29 +0300

    implement initializeStore function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export function add(a: number, b: number) {
-    return a + b;
+import { combineReducers, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+export function initializeStore(defaultStates: { [key: string]: any }, useDevTools: boolean = true) {
+    let reducers: { [key: string]: (state: any, action: { type: string, value: any }) => void } = {}
+    Object.keys(defaultStates).forEach(key => {
+        reducers[key] = (state, action) => {
+            return action.type === key ? action.value : state
+        }
+    })
+    return createStore(combineReducers(reducers), useDevTools ? composeWithDevTools() : undefined)
 }
