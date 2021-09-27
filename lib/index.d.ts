7af1154d3fceb74a95e548926b145ad6a7378daa
Shahar Ilany <shahar@sps.co.il>
2021-09-27 20:37:29 +0300

    publish 1.0.4

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,5 +2,6 @@ import { Store } from "redux";
 export declare function initializeStore(defaultStates: {
     [key: string]: any;
 }, useDevTools?: boolean): Store<any, import("redux").AnyAction>;
-export declare function setValue<T>(variable: keyof T): (newValue: any) => void;
+export declare function useGet<T, K extends keyof T = keyof T>(variable: K): T[K];
+export declare function setValue<T, K extends keyof T = keyof T>(variable: K): (newValue: T[K]) => void;
 //# sourceMappingURL=index.d.ts.map
