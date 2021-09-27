4dac8a0237b87543bddf8ab407319015a5a6b9c2
Shahar Ilany <shahar@sps.co.il>
2021-09-27 17:51:17 +0300

    add set value

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,6 @@
+import { Store } from "redux";
 export declare function initializeStore(defaultStates: {
     [key: string]: any;
-}, useDevTools?: boolean): import("redux").Store<import("redux").EmptyObject & {
-    [x: string]: void;
-}, {
-    type: string;
-    value: any;
-}>;
+}, useDevTools?: boolean): Store<any, import("redux").AnyAction>;
+export declare function setValue<T>(variable: keyof T): (newValue: any) => void;
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, Dispatch, Store } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
+var store: Store
+
+type action = { type: string, value: any }
+
 export function initializeStore(defaultStates: { [key: string]: any }, useDevTools: boolean = true) {
-    let reducers: { [key: string]: (state: any, action: { type: string, value: any }) => void } = {}
+    let reducers: { [key: string]: (state: any, action: action) => void } = {}
     Object.keys(defaultStates).forEach(key => {
         reducers[key] = (state = defaultStates[key], action) => {
             return action.type === key ? action.value : state
         }
     })
-    return createStore(combineReducers(reducers), useDevTools ? composeWithDevTools() : undefined)
+    store = createStore(combineReducers(reducers), useDevTools ? composeWithDevTools() : undefined)
+    return store
+}
+
+export function setValue<T>(variable: keyof T) {
+    return (newValue: any) => {
+        store.dispatch({
+            type: variable,
+            value: newValue
+        })
+    }
 }
