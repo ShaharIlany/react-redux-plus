8c4f635b5e37dd78ee5a2ed816876fbb9339caba
Shahar Ilany <shahar@sps.co.il>
2021-09-27 17:20:07 +0300

    implement store initialization

--- sample/src/index.tsx
+++ sample/src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { initializeStore } from 'react-redux-plus'
+import { Provider } from 'react-redux';
+
+const store = initializeStore({
+  paramA: "Test",
+  paramB: true
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
