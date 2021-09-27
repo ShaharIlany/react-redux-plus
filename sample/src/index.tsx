54d5403e866d946737b52de468e884300f483242
Shahar Ilany <shahar@sps.co.il>
2021-09-27 23:42:49 +0300

    add provider to main context

--- sample/src/index.tsx
+++ sample/src/index.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { initializeStore } from 'react-redux-plus'
+import { Provider } from 'react-redux'
 
 const initialStore = {
   paramA: "Test",
   paramB: true
 }
-initializeStore(initialStore)
+const store = initializeStore(initialStore)
 export type storeType = typeof initialStore
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
